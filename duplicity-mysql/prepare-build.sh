3cbf81e29b260d70dc854059f469b3238c21ca88
olivier.rochon <olivier.rochon@konvergence.com>
2026-01-20 08:27:58 +0100

    update tekton mysql

--- duplicity-mysql/prepare-build.sh
+++ duplicity-mysql/prepare-build.sh
@@ -21,10 +21,12 @@ MYSQL_VERSIONS="8.0"
 for MYSQL_VERSION in ${MYSQL_VERSIONS}; do
 
     export MYSQL_VERSION
+    export MYSQL_TAG_VERSION=${MYSQL_VERSION//./}
+
 
 # tags : * the tag will be [java-major-version].[tomcat-major-version].[tomcat-minor-version](-[tomcat-patch])(_[dockerfile-release](_(vendor-release)))
 DOCKER_IMAGE=duplicity
-DOCKER_RELEASE_TAG=${DUPLICITY_RELEASE}-mysql${MYSQL_VERSION//./}
+DOCKER_RELEASE_TAG=${DUPLICITY_RELEASE}-mysql${MYSQL_TAG_VERSION}
 
 
 # example: 21.9.0-98-temurin-jammy-r98, 21.9.0-98-temurin-jammy, 21.9.0-98, 21.9.0
@@ -34,15 +36,16 @@ TAG2=${DOCKER_RELEASE_TAG}
 BUILD_TAGS=()
 BUILD_TAGS+=("${TAG1}")
 BUILD_TAGS+=("${TAG2}")
-printf "%s\n" "${BUILD_TAGS[@]}" > mysql${MYSQL_VERSION//./}_build_tags.txt
+printf "%s\n" "${BUILD_TAGS[@]}" > mysql${MYSQL_TAG_VERSION}_build_tags.txt
 
 # Dockerfile ARGs for build
->mysql${MYSQL_VERSION//./}_build_args.txt
-echo ARG_KSHUTTLE_COPYRIGHT=Copyright $(date "+%Y") kShuttle - All rights reserved >> mysql${MYSQL_VERSION//./}_build_args.txt
-echo ARG_PG_RELEASE=${MYSQL_VERSION} >> mysql${MYSQL_VERSION//./}_build_args.txt
-echo ARG_RELEASE_TAG=${DOCKER_RELEASE_TAG} >> mysql${MYSQL_VERSION//./}_build_args.txt
-echo ARG_RELEASE_FULL_TAG=${TAG1} >> mysql${MYSQL_VERSION//./}_build_args.txt
+>mysql${MYSQL_TAG_VERSION}_build_args.txt
+echo ARG_KSHUTTLE_COPYRIGHT=Copyright $(date "+%Y") kShuttle - All rights reserved >> mysql${MYSQL_TAG_VERSION}_build_args.txt
+echo ARG_MYSQL_RELEASE=${MYSQL_VERSION} >> mysql${MYSQL_TAG_VERSION}_build_args.txt
+echo ARG_MYSQL_TAG_VERSION=${MYSQL_TAG_VERSION} >> mysql${MYSQL_TAG_VERSION}_build_args.txt
+echo ARG_RELEASE_TAG=${DOCKER_RELEASE_TAG} >> mysql${MYSQL_TAG_VERSION}_build_args.txt
+echo ARG_RELEASE_FULL_TAG=${TAG1} >> mysql${MYSQL_TAG_VERSION}_build_args.txt
 
 
-envsubst '${MYSQL_VERSION}'  < tekton-builder-dockerhub.dist > tekton-builder-mysql${MYSQL_VERSION//./}_dockerhub.yaml
+envsubst '${MYSQL_TAG_VERSION}'  < tekton-builder-dockerhub.dist > tekton-builder-mysql${MYSQL_TAG_VERSION}_dockerhub.yaml
 done
